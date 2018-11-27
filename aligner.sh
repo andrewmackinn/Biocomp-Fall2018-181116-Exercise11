840775b5b9ebd2f8c3a23bfa37c7276bc3d05506
Andrew MacKinnon <amackinn@nd.edu>
2018-11-27 16:13:13 -0500

    added comments

--- aligner.sh
+++ aligner.sh
@@ -1,8 +1,10 @@
+#usage bash aligner.sh
+#for loop to run sporecoats through muscle
 for file in sporecoat*.fasta
 do
 ~/local/bin/muscle3.8.31_i86linux64 -in $file -out align.$file
 done
-
+#for loop to run transporters through muscle
 for file in transporter*.fasta
 do
 ~/local/bin/muscle3.8.31_i86linux64 -in $file -out align.$file
